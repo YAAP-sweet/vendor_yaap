Project-Awaken/android_vendor_awaken

vendor: allow hiding navbar [2/3]
* overlay extracted from https://github.com/Magisk-Modules-Repo/HideNavBar. So credits goes to the developer of this module

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Change-Id: I241f486a56297e127ee730a1510ea0c22e4bd448

config/common.mk
@@ -167,6 +167,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# Inherit from rro_overlays config
+$(call inherit-product, vendor/awaken/config/rro_overlays.mk)
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/awaken/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/awaken/overlay/common
 

YAAP-sweet/vendor_yaap

Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

config/common.mk
@@ -129,6 +129,9 @@ OVERRIDE_TARGET_FLATTEN_APEX := true
 # # prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
 PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
 
+# Inherit from rro_overlays config
+$(call inherit-product, vendor/yaap/config/rro_overlays.mk)
+
 ifeq ($(TARGET_BUILD_GAPPS),true)
     $(call inherit-product-if-exists, vendor/google/gms/config.mk)
 endif