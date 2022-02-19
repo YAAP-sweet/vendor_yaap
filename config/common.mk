elpaablo/vendor_aosp

configs/common: add priv-app permissions for call recording
Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

config/common.mk
@@ -149,6 +149,10 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/permissions/privapp-permissions-google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google.xml
 endif
 
+# priv-app permissions
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/permissions/privapp-permissions-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-whitelist.xml
+    
 # Pixel sysconfig from Pixel XL (Photos)
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \

YAAP-sweet/vendor_yaap

config/common.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
     vendor/yaap/prebuilt/common/etc/permissions/yaap-privapp-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/yaap-privapp-permissions.xml \
     vendor/yaap/prebuilt/common/etc/permissions/yaap-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/yaap-power-whitelist.xml
 
+# priv-app permissions
+PRODUCT_COPY_FILES += \
+    vendor/yaap/prebuilt/common/etc/permissions/privapp-permissions-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-whitelist.xml
+
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
     vendor/yaap/prebuilt/common/etc/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml